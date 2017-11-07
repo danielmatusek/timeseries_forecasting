336264184c4d87d2525e708527b13d26aab16450
Sin <oka.spam@hi-leo.com>
2017-11-07 17:38:36 +0100

    dynamic Input

--- server.r
+++ server.r
@@ -15,6 +15,9 @@ ui <- dashboardPage(
 		checkboxInput('headerCheckbox', 'Header', TRUE),
 		radioButtons('separatorRadioButton', 'Separator',
 			c(Comma=',', Semicolon=';', Tab='\t', Space=' '), ','),
+		uiOutput("x_axis"),
+		uiOutput("y_axis"),
+		uiOutput("z_axis"),		
 		fileInput('dataFile', NULL,
 			accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')
 		),
@@ -24,8 +27,8 @@ ui <- dashboardPage(
 		uiOutput("meteridSelectBox"),
 		radioButtons('normalizationRadioButton', 'Normalization',
 			c('None' = 'none', 'Z-Normalization' = 'zScore', 'Min-Max Scale' = 'minmax'), 'none'),
-		sliderInput('windowSizeSlider', 'Window Size', 1, 30, 5, step = 1),
-		sliderInput('dataSplitSlider', 'Split Training/Test Data', 1, 100, 70, step = 1),
+		uiOutput('windowSizeSlider'),
+		sliderInput('dataSplitSlider', 'Split Training/Test Data', 1, 100, 70, post = " %", step = 1),
 
 		hr(),
 
@@ -69,15 +72,20 @@ ui <- dashboardPage(
 )
 
 server <- function(input, output) {
+  database_basis <- reactive({
+    file <- input$dataFile
+    
+    if (is.null(file))
+    {
+      return(NULL)
+    }
+    
+    df <- read.csv(file$datapath, header=input$headerCheckbox, sep=input$separatorRadioButton)
+  })
+  
 	database <- reactive({
-		file <- input$dataFile
-
-		if (is.null(file))
-		{
-			return(NULL)
-		}
-
-		df <- read.csv(file$datapath, header=input$headerCheckbox, sep=input$separatorRadioButton)
+		df <- database_basis()
+		if(is.null(df)) return(NULL)
 		split(df, f = df$meterid)
 	})
 
@@ -95,20 +103,49 @@ server <- function(input, output) {
 
 		if (normalization == 'zScore')
 		{
-			data.frame(day = df$day, consumption = scale(df$consumption))
+			data.frame(day = df[,x()], consumption = scale(df[,y()]))
 		}
 		else if (normalization == 'minmax')
 		{
-			maxs <- max(df$consumption)
-			mins <- min(df$consumption)
+			maxs <- max(df[,y()])
+			mins <- min(df[,y()])
 
-			data.frame(day = df$day, consumption = scale(df$consumption, center = mins, scale = maxs - mins))
+			data.frame(day = df[,x()], consumption = scale(df[,y()], center = mins, scale = maxs - mins))
 		}
 		else
 		{
-			data.frame(day = df$day, consumption = df$consumption)
+			data.frame(day = df[,x()], consumption = df[,y()])
 		}
 	})
+	
+	output$x_axis <- renderUI({ 
+	  df <- database_basis()
+	  if (is.null(df)) return()
+	  selectInput("x_axis", "x-Axis", names(df), selected = names(df)[2])
+	})
+	
+	output$y_axis <- renderUI({
+	  df <- database_basis()
+	  if (is.null(df)) return()
+	  selectInput("y_axis","y-Axis", names(df), selected = names(df)[3])
+	})
+	
+	x <- reactive({ input$x_axis })
+	
+	y <- reactive({ input$y_axis })
+	
+	output$windowSizeSlider <- renderUI({
+	  db <- database()
+	  
+	  if (is.null(db))
+	  {
+	    return(NULL)
+	  }
+	  meterid <- input$meteridSelect
+	  df <- db[[meterid]]
+	  sliderInput('windowSizeSlider', 'Window Size', 1, max(df[,x()])*0.1, max(df[,x()])*0.05, step = 1)
+	})	
+	
 	
 	windows <- reactive({
 	  df <- dataset()
@@ -119,7 +156,7 @@ server <- function(input, output) {
 	    return(NULL)
 	  }
 	  
-	  d <- as.data.frame(rollapply(df$consumption, width = windowSize+1, FUN = identity, by = 1), by.column = TRUE)
+	  d <- as.data.frame(rollapply(df[,y()], width = windowSize+1, FUN = identity, by = 1), by.column = TRUE)
 	  names(d) <- paste0('xt', 0:windowSize)
 	  d
 	})
@@ -181,14 +218,14 @@ server <- function(input, output) {
 	  if (normalization == 'zScore')
 	  {
 	    df <- db[[meterid]]
-	    scale <- sd(df$consumption)
-	    offset <- mean(df$consumption)
+	    scale <- sd(df[,y()])
+	    offset <- mean(df[,y()])
 	  }
 	  else if (normalization == 'minmax')
 	  {
 	    df <- db[[meterid]]
-	    maxs <- max(df$consumption)
-	    mins <- min(df$consumption)
+	    maxs <- max(df[,y()])
+	    mins <- min(df[,y()])
 	    
 	    scale <- maxs - mins
 	    offset <- mins
@@ -219,7 +256,7 @@ server <- function(input, output) {
 			return(NULL)
 		}
 
-		p <- plot_ly(df, x = ~day, y = ~consumption, type = 'scatter', mode = 'lines')
+		p <- plot_ly(df, x = df[,x()], y = df[,y()], type = 'scatter', mode = 'lines')
 		p$elementId <- NULL	# workaround for the "Warning in origRenderFunc() : Ignoring explicitly provided widget ID ""; Shiny doesn't use them"
 		p
 	})
