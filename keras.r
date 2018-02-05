df9b805e5caf2160a8d3a44dfe3563cfbb221291
Brandon Schmitt <Brandon.Schmitt@live.de>
2018-02-05 10:38:14 +0100

    lstm with batches

--- keras.r
+++ keras.r
@@ -6,50 +6,126 @@ library(RSNNS)
 use_condaenv("tensorflow")
 k <- backend()
 
-batch_size <<- 5
 
-trainLSTM <- function(id)
-{
-  model <- keras_model_sequential()
-  for (i in 1:length(vars$options$hiddenLayers))
+lookback <- 7
+delay <- 0
+batch_size <- 32
+minIndexTrain <- 1
+minIndexVal <- 1025
+minIndexTest <- 1281
+
+generator <- function(data, lookback, minIndex, maxIndex = NULL, shuffle = FALSE, delay = 0, batchSize = 128, step = 1) {
+  if (is.null(maxIndex))
   {
-    hiddenNeurons <- vars$options$hiddenLayers[i]
-    if (i == 1)
+    maxIndex <- nrow(data) - delay
+  }
+  
+  i <- minIndex + lookback
+  
+  function() {
+    if (shuffle)
     {
-      model %>% layer_dense(units = hiddenNeurons, input_shape = c(vars$options$windowSize))
+      rows <- sample(c((minIndex+lookback):maxIndex), size = batchSize)
     }
     else
     {
-      model %>% layer_dense(units = hiddenNeurons)
+      if (i + batchSize > maxIndex)
+      {
+        i <<- minIndex + lookback
+      }
+      rows <- c(i:min(i+batchSize-1, maxIndex))
+      i <<- i + length(rows)
+    }
+    samples <- array(0, dim = c(length(rows), lookback / step, dim(data)[[-1]]))
+    targets <- array(0, dim = c(length(rows)))
+    
+    for (j in 1:length(rows))
+    {
+      indices <- seq(rows[[j]] - lookback, rows[[j]] - 1, length.out = dim(samples)[[2]])
+      samples[j,,] <- data[indices,]
+      targets[[j]] <- data[rows[[j]] + delay, 1]
     }
     
-    model %>% layer_dropout(rate = 0.1)
+    list(samples, targets)
   }
-  model %>% layer_dense(units = vars$options$horizon)
+}
+
+
+trainLSTM <- function(id)
+{
+  #batch_size = 32
+  #timesteps = 8
+  #data_dim = 1
   
-  summary(model)
+  #browser()
+  #model <- keras_model_sequential()
+  #model %>% layer_lstm(32, activation = NULL, recurrent_activation = NULL, stateful = TRUE, return_sequences = TRUE,
+  #  input_shape = c(vars$options$windowSize, 16), batch_size = batch_size)
+  #model %>% layer_dense(1, activation = NULL)
   
-  model %>% compile(loss = loss_mean_absolute_percentage_error, optimizer = optimizer_rmsprop())
+  data <- data.matrix(vars$timeSeries[[id]][, -1])
+  trainData <- data[1:(minIndexVal-1),]
+  mean <- mean(trainData)
+  std <- sd(trainData)
+  data <- scale(data, center = mean, scale = std)
+  print(paste0('Mean = ', mean, ', std = ', std))
   
-  dataSet <- vars$timeSeries[[id]]$y
-  windows <- rollapply(dataSet, width = vars$options$windowSize + vars$options$horizon, FUN = identity, by = 1)
-  trainset <- windows[-NROW(windows),]
+  train_gen <- generator(data,
+    lookback = lookback,
+    delay = delay,
+    minIndex = minIndexTrain,
+    maxIndex = minIndexVal - 1,
+    shuffle = TRUE,
+    batchSize = batch_size)
+  val_gen <- generator(data,
+    lookback = lookback,
+    delay = delay,
+    minIndex = minIndexVal,
+    maxIndex = minIndexTest - 1,
+    batchSize = batch_size)
   
-  train.x <- trainset[, 1:vars$options$windowSize]
-  train.y <- trainset[, -(1:vars$options$windowSize)]
+  val_steps = (minIndexTest - minIndexVal - 1 - lookback) / batch_size
   
-  history <- model %>% fit(train.x, train.y, batch_size = 1)
+  model <- keras_model_sequential() %>%
+    layer_lstm(units = 32, input_shape = list(NULL, dim(data)[[-1]]), return_sequences = TRUE,
+      dropout = 0.2, recurrent_dropout = 0.5) %>%
+    layer_lstm(units = 64, return_sequences = TRUE, dropout = 0.2, recurrent_dropout = 0.5) %>%
+    layer_lstm(units = 128, activation = 'relu', dropout = 0.2, recurrent_dropout = 0.5) %>%
+    layer_dense(units = 1)
+  
+  model %>% compile(
+    optimizer = optimizer_rmsprop(),
+    #loss = 'mae'
+    loss = loss_mean_absolute_percentage_error
+  )
+  
+  history <- model %>% fit_generator(
+    train_gen,
+    steps_per_epoch = 500,
+    epochs = 20,
+    validation_data = val_gen,
+    validation_steps = val_steps
+  )
+  
+  model$mean = mean
+  model$std = std
   
   return(model)
 }
 
 getTestResults.lstm <- function(model, id)
 {
-  dataSet <- vars$timeSeries[[id]]$y
-  windows <- rollapply(dataSet, width = vars$options$windowSize + vars$options$horizon, FUN = identity, by = 1)
-  testset <- windows[NROW(windows), 1:vars$options$windowSize]
-  m <- matrix(c(testset), nrow = 1, ncol = vars$options$windowSize)
-  predict(model, m, batch_size = 1)[1, ]
+  data <- data.matrix(vars$timeSeries[[id]][, -1])
+  data <- scale(data, center = model$mean, scale = model$std)
+  test_gen <- generator(data,
+    lookback = lookback,
+    delay = delay,
+    minIndex = nrow(data) - vars$options$horizon,
+    batchSize = batch_size)
+  
+  test_steps = (nrow(data) - minIndexTest - 1 - lookback) / batch_size
+  predictions <- model %>% predict_generator(function() {list(test_gen()[[1]])}, steps = test_steps, verbose = TRUE)
+  return (predictions[,1]*model$std+model$mean)
 }
 
 trainLSTM.old.2 <- function(id)
