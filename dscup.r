a63c395c8b4d2d33cc220cfbac346977c3723f20
Daniel Matusek <da.matusek@gmail.com>
2018-02-07 21:33:56 +0100

    added functions for weekly and daily optimization

--- dscup.r
+++ dscup.r
@@ -11,10 +11,14 @@ startRoutine <- function(modelName, id, granularity){
         getBestMonthlyConfig(modelName, id)
     }
     else if (granularity == 'weekly'){
-        #todo
+        x <- data.frame("MODEL", "WINDOWSIZE", "LAYER ARCHITECTURE", "SMAPE")
+        write.table(x, file = "weekly.csv", append = TRUE, row.names = FALSE, col.names = FALSE, sep=";") 
+        getBestWeeklyConfig(modelName, id)
     }
     else if (granularity == 'daily'){
-        #todo
+        x <- data.frame("MODEL", "WINDOWSIZE", "LAYER ARCHITECTURE", "SMAPE")
+        write.table(x, file = "daily.csv", append = TRUE, row.names = FALSE, col.names = FALSE, sep=";") 
+        getBestDailyConfig(modelName, id)
     } else print('WARN: no such granularity (dscup.r)')
 }
 
@@ -60,11 +64,83 @@ getBestMonthlyConfig <- function(modelName, id){
 }
 
 getBestWeeklyConfig <- function(x){
-    #todo dasselbe wie beim monat nur mit anderm horizon
-    #write.csv(getBestMonthlyConfig(modelName, id), file = "weekly.csv", append = TRUE, eol = '\n')
+    vars$options$horizon <<- 4
+
+    #iteriere durch alle Windows und Layer
+    for(windowSize in 1:maxWindowSize){
+        vars$options$windowSize <<- windowSize
+        for(hiddenLayer in 1:maxHiddenLayer){
+            layerVector <- rep(1,hiddenLayer)
+            #j <- hiddenLayer
+            for(hiddenNodes in 1:((maxHiddenNodes ^ hiddenLayer)-1)){
+                #Aufbauen des HIdden Nodes Vektors
+                inc <- FALSE
+                j <- hiddenLayer
+                while(!inc){
+                    if (layerVector[j] == maxHiddenNodes){
+                        if(j==1) break
+                        layerVector[j] <- 1
+                        j <- j-1 
+                    } else {
+                        layerVector[j] <- (layerVector[j] + 1)
+                        inc <- TRUE
+                    }
+                    if(inc) break
+                }   
+
+                # Die eigentliche Verarbeitungsroutine in welcher  das Model gelernt wird 
+                # (in getTestResults und dann wird der SMAPE in eine Datei rausgeschrieben)
+                vars$options$hiddenLayers <<- layerVector
+                testResults <- getTestResults(modelName, id)
+                if(inherits(testResults, 'TestResults'))
+                {
+                    smape <- sMAPE(testResults$expected, testResults$predicted)  
+                    x <- data.frame(modelName, windowSize, toString(layerVector), smape)
+                    write.table(x, file = "monthly.csv", append = TRUE, row.names = FALSE, col.names = FALSE, sep=";") 
+                }
+                resetModels(modelName)
+            }
+        }
+    }   
 }
 
 getBestDailyConfig <- function(x){
-    #todo dasselbe wie beim monat nur mit anderm horizon
-    #write.csv(getBestMonthlyConfig(modelName, id), file = "daily.csv", append = TRUE, eol = '\n')
+    vars$options$horizon <<- 9
+
+    #iteriere durch alle Windows und Layer
+    for(windowSize in 1:maxWindowSize){
+        vars$options$windowSize <<- windowSize
+        for(hiddenLayer in 1:maxHiddenLayer){
+            layerVector <- rep(1,hiddenLayer)
+            #j <- hiddenLayer
+            for(hiddenNodes in 1:((maxHiddenNodes ^ hiddenLayer)-1)){
+                #Aufbauen des HIdden Nodes Vektors
+                inc <- FALSE
+                j <- hiddenLayer
+                while(!inc){
+                    if (layerVector[j] == maxHiddenNodes){
+                        if(j==1) break
+                        layerVector[j] <- 1
+                        j <- j-1 
+                    } else {
+                        layerVector[j] <- (layerVector[j] + 1)
+                        inc <- TRUE
+                    }
+                    if(inc) break
+                }   
+
+                # Die eigentliche Verarbeitungsroutine in welcher  das Model gelernt wird 
+                # (in getTestResults und dann wird der SMAPE in eine Datei rausgeschrieben)
+                vars$options$hiddenLayers <<- layerVector
+                testResults <- getTestResults(modelName, id)
+                if(inherits(testResults, 'TestResults'))
+                {
+                    smape <- sMAPE(testResults$expected, testResults$predicted)  
+                    x <- data.frame(modelName, windowSize, toString(layerVector), smape)
+                    write.table(x, file = "monthly.csv", append = TRUE, row.names = FALSE, col.names = FALSE, sep=";") 
+                }
+                resetModels(modelName)
+            }
+        }
+    } 
 }
