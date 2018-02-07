e393474ab9f041c2557271d6008c42f78d56e1a5
Daniel Matusek <da.matusek@gmail.com>
2018-02-07 21:38:55 +0100

    shorten code

--- dscup.r
+++ dscup.r
@@ -2,28 +2,35 @@ maxWindowSize <<- 2
 maxHiddenLayer <<- 2
 maxHiddenNodes <<- 2
 
+horizon.monthly <<- 3
+horizon.weekly <<- 4
+horizon.daily <<- 9
+
+filename <<- NULL
+
 startRoutine <- function(modelName, id, granularity){
     #write.csv()
     loadedID <- names(vars$timeSeries)
+    x <- data.frame("TS-ID", "MODEL", "WINDOWSIZE", "LAYER ARCHITECTURE", "SMAPE")
     if(granularity == 'monthly'){
-        x <- data.frame("MODEL", "WINDOWSIZE", "LAYER ARCHITECTURE", "SMAPE")
-        write.table(x, file = "monthly.csv", append = TRUE, row.names = FALSE, col.names = FALSE, sep=";") 
-        getBestMonthlyConfig(modelName, id)
+        filename <<- 'monthly.csv'
+        write.table(x, file = filename, append = TRUE, row.names = FALSE, col.names = FALSE, sep=";") 
+        getBestConfig(modelName, id, horizon.monthly)
     }
     else if (granularity == 'weekly'){
-        x <- data.frame("MODEL", "WINDOWSIZE", "LAYER ARCHITECTURE", "SMAPE")
-        write.table(x, file = "weekly.csv", append = TRUE, row.names = FALSE, col.names = FALSE, sep=";") 
-        getBestWeeklyConfig(modelName, id)
+        filename <<- 'weekly.csv'
+        write.table(x, file = filename, append = TRUE, row.names = FALSE, col.names = FALSE, sep=";") 
+        getBestConfig(modelName, id, horizon.weekly)
     }
     else if (granularity == 'daily'){
-        x <- data.frame("MODEL", "WINDOWSIZE", "LAYER ARCHITECTURE", "SMAPE")
-        write.table(x, file = "daily.csv", append = TRUE, row.names = FALSE, col.names = FALSE, sep=";") 
-        getBestDailyConfig(modelName, id)
-    } else print('WARN: no such granularity (dscup.r)')
+        filename <<- 'daily.csv'
+        write.table(x, file = filename, append = TRUE, row.names = FALSE, col.names = FALSE, sep=";") 
+        getBestConfig(modelName, id, horizon.daily)
+    } else print('USERWARN: no such granularity (dscup.r)')
 }
 
-getBestMonthlyConfig <- function(modelName, id){
-    vars$options$horizon <<- 3
+getBestConfig <- function(modelName, id, horizon){
+    vars$options$horizon <<- horizon
 
     #iteriere durch alle Windows und Layer
     for(windowSize in 1:maxWindowSize){
@@ -54,8 +61,8 @@ getBestMonthlyConfig <- function(modelName, id){
                 if(inherits(testResults, 'TestResults'))
                 {
                     smape <- sMAPE(testResults$expected, testResults$predicted)  
-                    x <- data.frame(modelName, windowSize, toString(layerVector), smape)
-                    write.table(x, file = "monthly.csv", append = TRUE, row.names = FALSE, col.names = FALSE, sep=";") 
+                    x <- data.frame(id, modelName, windowSize, toString(layerVector), smape)
+                    write.table(x, file = filename, append = TRUE, row.names = FALSE, col.names = FALSE, sep=";") 
                 }
                 resetModels(modelName)
             }
@@ -63,84 +70,84 @@ getBestMonthlyConfig <- function(modelName, id){
     }                
 }
 
-getBestWeeklyConfig <- function(x){
-    vars$options$horizon <<- 4
+# getBestWeeklyConfig <- function(x){
+#     vars$options$horizon <<- 4
 
-    #iteriere durch alle Windows und Layer
-    for(windowSize in 1:maxWindowSize){
-        vars$options$windowSize <<- windowSize
-        for(hiddenLayer in 1:maxHiddenLayer){
-            layerVector <- rep(1,hiddenLayer)
-            #j <- hiddenLayer
-            for(hiddenNodes in 1:((maxHiddenNodes ^ hiddenLayer)-1)){
-                #Aufbauen des HIdden Nodes Vektors
-                inc <- FALSE
-                j <- hiddenLayer
-                while(!inc){
-                    if (layerVector[j] == maxHiddenNodes){
-                        if(j==1) break
-                        layerVector[j] <- 1
-                        j <- j-1 
-                    } else {
-                        layerVector[j] <- (layerVector[j] + 1)
-                        inc <- TRUE
-                    }
-                    if(inc) break
-                }   
+#     #iteriere durch alle Windows und Layer
+#     for(windowSize in 1:maxWindowSize){
+#         vars$options$windowSize <<- windowSize
+#         for(hiddenLayer in 1:maxHiddenLayer){
+#             layerVector <- rep(1,hiddenLayer)
+#             #j <- hiddenLayer
+#             for(hiddenNodes in 1:((maxHiddenNodes ^ hiddenLayer)-1)){
+#                 #Aufbauen des HIdden Nodes Vektors
+#                 inc <- FALSE
+#                 j <- hiddenLayer
+#                 while(!inc){
+#                     if (layerVector[j] == maxHiddenNodes){
+#                         if(j==1) break
+#                         layerVector[j] <- 1
+#                         j <- j-1 
+#                     } else {
+#                         layerVector[j] <- (layerVector[j] + 1)
+#                         inc <- TRUE
+#                     }
+#                     if(inc) break
+#                 }   
 
-                # Die eigentliche Verarbeitungsroutine in welcher  das Model gelernt wird 
-                # (in getTestResults und dann wird der SMAPE in eine Datei rausgeschrieben)
-                vars$options$hiddenLayers <<- layerVector
-                testResults <- getTestResults(modelName, id)
-                if(inherits(testResults, 'TestResults'))
-                {
-                    smape <- sMAPE(testResults$expected, testResults$predicted)  
-                    x <- data.frame(modelName, windowSize, toString(layerVector), smape)
-                    write.table(x, file = "monthly.csv", append = TRUE, row.names = FALSE, col.names = FALSE, sep=";") 
-                }
-                resetModels(modelName)
-            }
-        }
-    }   
-}
+#                 # Die eigentliche Verarbeitungsroutine in welcher  das Model gelernt wird 
+#                 # (in getTestResults und dann wird der SMAPE in eine Datei rausgeschrieben)
+#                 vars$options$hiddenLayers <<- layerVector
+#                 testResults <- getTestResults(modelName, id)
+#                 if(inherits(testResults, 'TestResults'))
+#                 {
+#                     smape <- sMAPE(testResults$expected, testResults$predicted)  
+#                     x <- data.frame(modelName, windowSize, toString(layerVector), smape)
+#                     write.table(x, file = "monthly.csv", append = TRUE, row.names = FALSE, col.names = FALSE, sep=";") 
+#                 }
+#                 resetModels(modelName)
+#             }
+#         }
+#     }   
+# }
 
-getBestDailyConfig <- function(x){
-    vars$options$horizon <<- 9
+# getBestDailyConfig <- function(x){
+#     vars$options$horizon <<- 9
 
-    #iteriere durch alle Windows und Layer
-    for(windowSize in 1:maxWindowSize){
-        vars$options$windowSize <<- windowSize
-        for(hiddenLayer in 1:maxHiddenLayer){
-            layerVector <- rep(1,hiddenLayer)
-            #j <- hiddenLayer
-            for(hiddenNodes in 1:((maxHiddenNodes ^ hiddenLayer)-1)){
-                #Aufbauen des HIdden Nodes Vektors
-                inc <- FALSE
-                j <- hiddenLayer
-                while(!inc){
-                    if (layerVector[j] == maxHiddenNodes){
-                        if(j==1) break
-                        layerVector[j] <- 1
-                        j <- j-1 
-                    } else {
-                        layerVector[j] <- (layerVector[j] + 1)
-                        inc <- TRUE
-                    }
-                    if(inc) break
-                }   
+#     #iteriere durch alle Windows und Layer
+#     for(windowSize in 1:maxWindowSize){
+#         vars$options$windowSize <<- windowSize
+#         for(hiddenLayer in 1:maxHiddenLayer){
+#             layerVector <- rep(1,hiddenLayer)
+#             #j <- hiddenLayer
+#             for(hiddenNodes in 1:((maxHiddenNodes ^ hiddenLayer)-1)){
+#                 #Aufbauen des HIdden Nodes Vektors
+#                 inc <- FALSE
+#                 j <- hiddenLayer
+#                 while(!inc){
+#                     if (layerVector[j] == maxHiddenNodes){
+#                         if(j==1) break
+#                         layerVector[j] <- 1
+#                         j <- j-1 
+#                     } else {
+#                         layerVector[j] <- (layerVector[j] + 1)
+#                         inc <- TRUE
+#                     }
+#                     if(inc) break
+#                 }   
 
-                # Die eigentliche Verarbeitungsroutine in welcher  das Model gelernt wird 
-                # (in getTestResults und dann wird der SMAPE in eine Datei rausgeschrieben)
-                vars$options$hiddenLayers <<- layerVector
-                testResults <- getTestResults(modelName, id)
-                if(inherits(testResults, 'TestResults'))
-                {
-                    smape <- sMAPE(testResults$expected, testResults$predicted)  
-                    x <- data.frame(modelName, windowSize, toString(layerVector), smape)
-                    write.table(x, file = "monthly.csv", append = TRUE, row.names = FALSE, col.names = FALSE, sep=";") 
-                }
-                resetModels(modelName)
-            }
-        }
-    } 
-}
+#                 # Die eigentliche Verarbeitungsroutine in welcher  das Model gelernt wird 
+#                 # (in getTestResults und dann wird der SMAPE in eine Datei rausgeschrieben)
+#                 vars$options$hiddenLayers <<- layerVector
+#                 testResults <- getTestResults(modelName, id)
+#                 if(inherits(testResults, 'TestResults'))
+#                 {
+#                     smape <- sMAPE(testResults$expected, testResults$predicted)  
+#                     x <- data.frame(modelName, windowSize, toString(layerVector), smape)
+#                     write.table(x, file = "monthly.csv", append = TRUE, row.names = FALSE, col.names = FALSE, sep=";") 
+#                 }
+#                 resetModels(modelName)
+#             }
+#         }
+#     } 
+# }
