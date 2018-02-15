efb6581f026cb3b16a5ba83a96c3fad992890045
Daniel Matusek <da.matusek@gmail.com>
2018-02-15 10:27:30 +0100

    added comments to elman

--- elman.r
+++ elman.r
@@ -2,7 +2,9 @@
  
  validationset.ratio <<- 0.05
  
- 
+# trains a Elman network of the RSNNS package
+# --id: ID of the time series
+# --hiddenLayers: vector of the hidden layers 
 trainRNN <- function(id, hiddenLayers = c(0))
 {
   set.seed(1)
@@ -19,11 +21,16 @@ trainRNN <- function(id, hiddenLayers = c(0))
   return(rnn)
 }
 
+# get the model for the Elman network of the RSNNS package
+# --id: id of the time series
 getModel.elman <- function(id)
 {
   trainRNN(id, vars$options$hiddenLayers)
 }
 
+# get the test results for a Elman network
+# --model: the model object
+# --id: id of the time series
 getTestResults.elman <- function(model, id)
 {
   testSet <- getTestSet(id, normalization = '0_1')
